55ef1dc66c28a0cb6b271e9a143827139fe2156c
Zah Loua <126197388+zaho-lou@users.noreply.github.com>
2023-04-23 20:49:20 +0100

    Update VisualisationGraph.py

--- VisualisationGraph.py
+++ VisualisationGraph.py
@@ -3,12 +3,19 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
+import warnings
+import matplotlib.pyplot as plt
+warnings.filterwarnings("ignore")
+plt.style.use('fivethirtyeight')
+
+
 
 # Connexion à la base de données
+
 conn = sqlite3.connect('ma_base.db')
 c = conn.cursor()
 
-# Fonction pour récupérer les données du client pour l'année spécifiée
+    # Fonction pour récupérer les données du client pour l'année spécifiée
 def get_customer_data(client_id, year):
     query = f"SELECT date, valeur FROM ma_table WHERE client_id = {client_id} AND strftime('%Y', date) = '{year}'"
     result = c.execute(query).fetchall()
@@ -23,7 +30,7 @@ def transform_data(data):
 
 # Fonction pour afficher le graphique
 def show_plot(data):
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15,5))
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(28,11))
     fig.suptitle('Consommations mensuelles pour l\'année sélectionnée')
     ax1.bar(data['date'], data['valeur'], color='purple')
     ax1.set_title('Barres')
@@ -43,10 +50,11 @@ client_id = st.sidebar.selectbox("Sélectionnez un client :", df['client_id'].un
 
 now = datetime.now()
 year = st.sidebar.selectbox('Sélectionnez une année', range(now.year - 17, now.year + 1))
-if st.button('Afficher les données'):
-    data = get_customer_data(client_id, year)
-    df = transform_data(data)
-    show_plot(df)
+
+data = get_customer_data(client_id, year)
+df = transform_data(data)
+show_plot(df)
 
 # Fermer la connexion à la base de données
+c.close()
 conn.close()

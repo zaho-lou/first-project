95c9721fa8072f0deaf05a524d9097b19bbc8ad4
Zah Loua <126197388+zaho-lou@users.noreply.github.com>
2023-04-28 03:14:28 +0100

    Add files via upload

--- visualisationTab.py
+++ visualisationTab.py
@@ -1,38 +1,57 @@
 import streamlit as st
 import sqlite3
+import pandas as pd
 
-# Connexion à la base de données SQLite3
-conn = sqlite3.connect('ma_base.db')
-cur = conn.cursor()
+def main():
 
-# Interface utilisateur Streamlit
-st.title("Visualisation des consommations annuelles et mensuelles des clients")
+    conn = sqlite3.connect('ma_base1.db')
+    cur = conn.cursor()
 
-# Saisie de l'ID du client
-client_id = st.number_input("Saisir l\'ID du client :", min_value=0, max_value=10000)
+    # Interface utilisateur Streamlit
+    st.title("Visualisation des consommations des clients")
 
-# Récupération des consommations annuelles du client
-cur.execute("SELECT strftime('%Y', date), SUM(valeur) FROM ma_table WHERE client_id = ? GROUP BY strftime('%Y', date)", (client_id,))
-rows_an = cur.fetchall()
+    # Récupération de tous les IDs clients existants dans la table
+    cur.execute("SELECT DISTINCT client_id FROM ma_table")
+    clients = cur.fetchall()
 
-# Récupération des consommations mensuelles du client
-cur.execute("SELECT strftime('%Y-%m', date), SUM(valeur) FROM ma_table WHERE client_id = ? GROUP BY strftime('%Y-%m', date)", (client_id,))
-rows_mois = cur.fetchall()
+    # Saisie de l'ID du client
+    client_id = st.selectbox("Saisir l'ID du client", [client[0] for client in clients])
 
-# Affichage des consommations annuelles sous forme de tableau
-if rows_an:
-    st.write(f"Consommations annuelles du client {client_id}")
-    st.table(rows_an)
-else:
-    st.warning(f"Aucune consommation annuelle trouvée pour le client {client_id}")
+    # Récupération des consommations annuelles du client
+    cur.execute("SELECT strftime('%Y', date) AS annee, round(SUM(valeur)) FROM ma_table WHERE client_id = ? GROUP BY strftime('%Y', date)", (client_id,))
+    rows_an = cur.fetchall()
 
-# Affichage des consommations mensuelles sous forme de tableau
-if rows_mois:
-    st.write(f"Consommations mensuelles du client {client_id}")
-    st.table(rows_mois)
-else:
-    st.warning(f"Aucune consommation mensuelle trouvée pour le client {client_id}")
+    # Récupération des consommations mensuelles du client
+    cur.execute("SELECT strftime('%Y', date) || ' ' || strftime('%m', date) AS mois, round(SUM(valeur)) FROM ma_table WHERE client_id = ? GROUP BY strftime('%Y-%m', date)", (client_id,))
+    rows_mois = cur.fetchall()
 
-# Fermeture de la connexion à la base de données
-cur.close()
-conn.close()
+    # Fermeture de la connexion à la base de données
+    cur.close()
+    conn.close()
+
+    # Création du dataframe pour les consommations annuelles
+    df_an = pd.DataFrame(rows_an, columns=['annee', 'consommation_annuelle'])
+
+    # Création du dataframe pour les consommations mensuelles
+    df_mois = pd.DataFrame(rows_mois, columns=['mois', 'consommation_mensuelle'])
+
+    # Formatage des données pour le dataframe final
+
+    df_mois['annee'] = df_mois['mois'].str[:4]
+    df_mois['mois'] = df_mois['mois'].str[-2:]
+    df_mois['mois'] = pd.to_datetime(df_mois['mois'], format='%m').dt.month_name(locale='fr_FR')
+    df = df_mois.pivot(index='annee', columns='mois', values='consommation_mensuelle')
+
+
+    # Réorganisation des colonnes
+    cols = list(df.columns)
+    df = df[cols]
+
+    # Renommage des index et colonnes
+    df.index.name = 'Année'
+    df.columns = [col.capitalize() for col in df.columns]
+
+
+    # Affichage du dataframe final
+st.write(f"Consommations du client {int(client_id)}")
+st_table = st.experimental_data_editor(df, num_rows="dynamic")
